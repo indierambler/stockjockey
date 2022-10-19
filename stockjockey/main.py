91cd9fde86ac3475eaad8c93c6b3de558d1785f4
indierambler <bryanjtimm@gmail.com>
2022-10-19 18:23:38 +0200

    feat: init flask factory and blueprints

---
+++ stockjockey/main.py
@@ -0,0 +1,48 @@
+# Import dependencies
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.exceptions import abort
+from stockjockey.auth import login_required
+from stockjockey.db import get_db
+
+
+# Create blueprint
+bp = Blueprint('main', __name__)
+
+
+@bp.route('/')
+def dashboard():
+    db = get_db()
+    posts = db.execute(
+        'SELECT * FROM asset'
+    ).fetchall()
+    return render_template('main/dashboard.html', posts=posts)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        ticker = request.form['ticker']
+        exchange = request.form['exchange']
+        sector = request.form['sector']
+        error = None
+
+        if not ticker:
+            error = 'Ticker is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO asset (ticker, exchange, sector)'
+                ' VALUES (?, ?, ?)',
+                (ticker, exchange, sector)
+            )
+            db.commit()
+            return redirect(url_for('main.dashboard'))
+
+    return render_template('main/create.html')
