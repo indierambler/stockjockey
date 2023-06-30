a64d81c322d35c89816f2fa479fb861df54c02f5
indierambler <bryanjtimm@gmail.com>
2023-06-30 11:39:26 -0400

    feature(stocksnap): initial blueprint

---
+++ stockjockey/stocksnap/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+stocksnap_bp = Blueprint('stocksnap', __name__, template_folder='templates')
+
+from . import views
---
+++ stockjockey/stocksnap/views.py
@@ -0,0 +1,100 @@
+# Import dependencies
+from . import stocksnap_bp
+import functools
+import os
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app
+)
+from werkzeug.exceptions import abort
+from stockjockey.auth import login_required
+from stockjockey.db import get_db, init_db, query_db
+
+
+@stocksnap_bp.route('/stock/<ticker>', methods=('GET', 'POST'))
+@login_required
+def snapshot(ticker=None):
+    # Search form
+    if request.method == 'POST':
+        # process ticker input
+        ticker = request.form['ticker']
+        error = None
+        if not ticker:
+            error = 'Ticker is required.'
+            flash(error)
+        else:
+            if request.form['submit'] == 'Search':
+                # "search" button - open the ticker snapshot page
+                return redirect(url_for('main.stocksnap.snapshot', ticker=ticker))
+            elif request.form['submit'] == 'Add':
+                # "add" button - add the ticker to watchlist
+                query_db(
+                    f"INSERT INTO asset (ticker)"
+                    f" VALUES ('{ticker}')"
+                )
+            elif request.form['submit'] == 'Remove':
+                # "remove" button - remove the ticker from watchlist
+                query_db(
+                    f"DELETE FROM asset"
+                    f" WHERE ticker = '{ticker}'"
+                )
+    
+    # Update watchlist form
+    
+    # process ticker input
+    if ticker:
+        # load stock template
+        pass
+    else:
+        # load not found template
+        pass
+
+
+    
+    return render_template('stocksnap/snapshot.html', ticker=ticker)
+
+
+@stocksnap_bp.route('/search', methods=('GET', 'POST'))
+@login_required
+def search():
+    if request.method == 'POST':
+        ticker = request.form['ticker']
+        error = None
+
+        if not ticker:
+            error = 'Ticker is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            query_db(
+                f"INSERT INTO asset (ticker)"
+                f" VALUES ('{ticker}')"
+            )
+            return redirect(url_for('main.dashboard'))
+
+    return render_template('stocksnap/search.html')
+
+
+def get_post(id, check_author=True):
+    post = query_db(
+        'SELECT p.id, name, ticker'
+        ' FROM post p'
+        ' WHERE p.id = ?',
+        (id,)
+    )[0]
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@stocksnap_bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = query_db('DELETE FROM post WHERE id = ?', (id,))
+    return redirect(url_for('main.dashboard'))
