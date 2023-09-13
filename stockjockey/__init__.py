e0c084f9440cf5691da38cf399a1825ace00cc89
indierambler <bryanjtimm@gmail.com>
2023-09-13 19:20:21 -0400

    chore: build db url within config

--- stockjockey/__init__.py
+++ stockjockey/__init__.py
@@ -2,6 +2,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import import_string
 
 
 # App Factory #
@@ -12,9 +13,9 @@ def create_app(test_config=None):
     # Configure the App #
     # load default and public config corresponding to current deployment env
     if os.getenv('DEPLOYMENT'):
-        config_str = f"config.Config{os.getenv('DEPLOYMENT').capitalize()}"
+        config_str = import_string(f"config.Config{os.getenv('DEPLOYMENT').capitalize()}")()
     else:
-        config_str = "config.Config"
+        config_str = import_string("config.Config")()
     app.config.from_object(config_str)
     # load private config values from env
     # app.config.from_envvar('PRIVATE_CONFIG', default='')
